c4c776c67c3abbd6ad02badb61fe115630a006a5
Rabeya Begum <shahnewaz@mail.com>
2021-02-07 20:36:09 +0600

    pushing

--- monster.js
+++ monster.js
@@ -12,58 +12,37 @@ const getMealName = meal => {
 const displayMeals = meals => {
     const mealsDiv = document.getElementById('meals');
     meals.forEach(meal => {
-        
+
         const mealDiv = document.createElement('div');
         mealDiv.className = 'meal-decor'
         const mealsInfo = `
         <img src="${meal.strMealThumb}"></img>
         <h3 class="meal-name">${meal.strMeal}</h3>
+        <button onclick="displayMealDetail('${meal.strMeal}')" >Details</button>
         `
         mealDiv.innerHTML = mealsInfo;
         mealsDiv.appendChild(mealDiv);
     });
-    
-    //for (let i = 0; i < meals.length; i++) {
-       // 
-        
-
-
-
-
-
-
-
-
-        // const img = document.createElement('img');
-        // img.innerText = meal.strMealThumb;
-        // const h3 = document.createElement('h3');
-        // h3.innerText = meal.strMeal;
-        // mealDiv.appendChild(img);
-        // mealDiv.appendChild(h3);
-         
-        // console.log(meal.strMealThumb);
-
-    
-
-
-
-
-
-
-
-    // const mealsDiv = document.getElementById('meals');
-    // meals.forEach(meal => {
-    //     const mealDiv = document.createElement('div');
-    //     mealDiv.className = 'meal';
-    //     const mealInfo = `
-    //         <h3 class="meal-name">${meals.strMeal}</h3>
-    //         <p>${meals.strCategory}</p>
-    //         <button onclick="displayMealsDetail('${meals.strMeal}')">Details</button>
-    //     `;
-    //     mealDiv.innerHTML = mealInfo;
-    //     mealsDiv.appendChild(mealDiv);
-    // });
 }
+displayMealDetail = meal => {
+    const url = `${apiBase}/${apiKey}/search.php?s=${meal}`
+    fetch(url)
+    
+    .then(response => response.json())
+    .then(data => renderMealInfo(data.meal));
+}
+const renderMealInfo = meal => {
+    const mealDiv = document.getElementById('meal-detail');
+    mealDiv.innerHTML = `
+    <img src="${meal.strMealThumb}"></img>
+    <p>Ingridents: ${meal.strIngredient1}</p>
+    <p>Ingridents: ${meal.strIngredient2}</p>
+    <p>Ingridents: ${meal.strIngredient3}</p>
+    <p>Ingridents: ${meal.strIngredient4}</p>
+    <p>Ingridents: ${meal.strIngredient5}</p>
+    `
+}
+
 
 
 const searchBtn = document.getElementById('search_button');
@@ -72,14 +51,3 @@ searchBtn.addEventListener('click', () => {
     getMealName(inputMeal)
 })
 
-
-
-
-
-// const updateUI = data => {
-//     document.getElementById('show_city').innerText = data.name || "Unknown Location!";
-//     document.getElementById('show_temperature').innerText = data.main.temp;
-//     document.getElementById('weather_status').innerText = data.weather[0].main;
-//     document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-//     document.getElementById('city').value = "";
-// }
